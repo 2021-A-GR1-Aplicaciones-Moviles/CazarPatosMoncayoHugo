3f47faeaa2203476f73db5e8f08853309c5fc819
Hugo Moncayo <hugo.moncayo@epn.edu.ec>
2021-07-07 19:28:43 -0500

    Creacion de Proyecto
    Proyecto Inicial Creado

---
+++ app/src/main/java/com/epnfis/cazarpatos/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.epnfis.cazarpatos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //Toast.makeText(this,"Saludos",Toast.LENGTH_LONG).show()
+        Toast.makeText(this, getString(R.string.saludos) ,Toast.LENGTH_LONG).show()
+    }
+}
